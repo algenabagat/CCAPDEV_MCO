f917881d1830a229f9d12eb72cde9ac902bd8ee8
algen_abagat <144084800+algenabagat@users.noreply.github.com>
2025-07-04 14:06:51 +0800

    mongodb installation

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.listen(3000);
+
+ mongoose.connect('mongodb://localhost:27017/labReservation', {
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+ });
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
     });
