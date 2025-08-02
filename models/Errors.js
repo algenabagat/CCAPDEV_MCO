5a9444fb5fb64c7af144593bf871135564972d82
algen_abagat <144084800+algenabagat@users.noreply.github.com>
2025-08-02 23:19:44 +0800

    update

--- models/Errors.js
+++ models/Errors.js
@@ -4,15 +4,15 @@ const errorLogSchema = new mongoose.Schema({
   message: String,
   stack: String,
   name: String,
-  code: String, // optional
+  code: String,
   timestamp: { type: Date, default: Date.now },
   location: String,
-  route: String, // optional
-  method: String, // optional
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // optional
-  requestBody: mongoose.Schema.Types.Mixed, // optional
-  queryParams: mongoose.Schema.Types.Mixed, // optional
-  params: mongoose.Schema.Types.Mixed // optional
+  route: String,
+  method: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  requestBody: mongoose.Schema.Types.Mixed,
+  queryParams: mongoose.Schema.Types.Mixed,
+  params: mongoose.Schema.Types.Mixed
 });
 
 module.exports = mongoose.model('ErrorLog', errorLogSchema);
